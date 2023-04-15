990c8766fa7b4e64584f60332c6adc815db7a55a
ayoubgots <1ayoub.semlali@gmail.com>
2023-04-15 10:02:31 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpItem from "./components/ExpenseItem"
+
+const App = () =>{
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpItem> </ExpItem>
+    </div>
+  );
+}
+
+export default App;
